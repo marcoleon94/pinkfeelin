c91d86b9b6e173cfba8032a7be2433f27a881db8
marcoleon <paramore_malj@outlook.com>
2016-09-09 16:36:22 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <link rel="stylesheet" href="css/materialize.css">
+ <head>
+   <title>Hello World</title>
+ </head>
+ <body class="pink lighten-5">
+     <div class="container">
+     <div class="pink accent-1 center s6">
+        <h2>Pink Feeli'n</h2>
+     </div>
+     </div>
+     
+ </body>
+</html>
