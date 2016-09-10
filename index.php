5e138e48fe7da039ad860e2ea071fd20230290ef
marcoleon <paramore_malj@outlook.com>
2016-09-10 15:44:27 -0500

    cambios al menu y a index

--- index.php
+++ index.php
@@ -9,15 +9,46 @@
          <div class="center">
             <h2>El Detalle Perfecto</h2>
             <div class="divider"></div>
-             <div class="row">
+             <div class="row" style="margin:4%;">
                  <div class="col s9">
-                     <p> blajdvlkajsfdlkasjdfalsfjalsjfkalsjflkasjdfasjkdflakjsdflkasjdfakjdlakjdflkaksjdflaksjdflaksdjflkasjdflkasjdfkaljdfakjdalksjdflaksjfdlakjsfalksjf</p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum lectus sit amet accumsan auctor. Fusce vel nunc rutrum, posuere magna vitae, iaculis velit. Mauris nec risus sodales, ullamcorper tortor quis, interdum ipsum. Vestibulum pulvinar fringilla ultrices. Etiam efficitur, purus ac consectetur rutrum, libero purus laoreet odio, in rhoncus erat purus id dolor. Aliquam auctor nulla eget pretium pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque at pulvinar dui, non egestas dolor. Praesent pretium est metus, sit amet tincidunt enim congue eu. Vivamus pharetra purus eget orci posuere, ac maximus dolor ornare. Duis vitae semper elit, in dictum urna. Nunc vitae metus lacus. Suspendisse quis est fringilla, posuere turpis non, cursus tortor. Fusce venenatis non justo sit amet vehicula. Maecenas vel justo ut turpis luctus ornare. Phasellus ullamcorper egestas dolor, in sollicitudin odio.
                  </div>
                  <div class="col s3"> 
-                     asdfjalñsfdañljflakshfjaksdhfakjshflkashflkasjhfkaljhfaksjlhsafasaksdfafakhfkahdfkadhfkadhfakjhfklahdfaksjhdfkajsdhflkasjhfkljashfaljkshfdklajshf
+                     <h3  class="thin">Accesosrios </h3><h3 class="bold">Y ROPA</h3>
+                     <p >
+                        Párrafo. Haz clic aquí para agregar tu propio texto y editar.
+                        Es fácil. Haz clic en Editar Texto o doble clic aquí para agregar tu contenido y cambiar la fuente. En este espacio puedes contar tu historia y permitir a tus usuarios saber más sobre ti.
+                     </p>
+                     <div class="row" style=" margin:3%">
+                     <a href="https://www.facebook.com/PinkFeelin/"><img class="responsive-img" src="images/facebook.png" height="15%" width="15%"></a>
+                     <a href="https://plus.google.com/"><img class="responsive-img" src="images/googleplus.png" height="15%" width="15%"></a>
+                     <a href="https://es.pinterest.com/"><img class="responsive-img" src="images/pinterest.png" height="15%" width="15%"></a>
+                     <a href="https://www.tumblr.com/"><img class="responsive-img" src="images/tumblr.png" height="15%" width="15%"></a>
+                     <a href="https://twitter.com/"><img class="responsive-img" src="images/twitter.png" height="15%" width="15%"></a>
+                     </div>
+                     <div class="divider"></div>
+                     <h5 class="thin">Recibie noticias de nuestras ofertas y nuestra nueva mercancía:</h5>
+                     <form>
+                         <div class="row">
+                            <div class="input-field col s12">
+                              <input id="email" type="email" class="validate">
+                              <label class="left-align"for="email">Email</label>
+                            </div>
+                             <div>
+                             <button class="btn waves-effect waves-light" type="submit" name="action">Suscribete Ya!
+                                <i class="material-icons right">send</i>
+                              </button>
+                             </div>
+                          </div>
+                     </form>
                  </div>
-                 
              </div>
+             <div class="row" style="margin:4%;">
+                <div class="col s5">
+                    <h4 class="bold">Todo lo que necesitas para actualizar tu look.</h4>
+                 </div>
+             </div>
+             
          </div>
      </div>
  </body>
--- menu.php
+++ menu.php
@@ -1,10 +1,20 @@
 <html lang="es">
+   <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
     <body>
-        <div class="navbar-fixed">
+        <!--<div class="navbar-fixed">-->
         <nav class="pink lighten-3">
             <div class="nav-wrapper">
-              <a href="#!" class="right brand-logo"><img class="responsive-img" src="images/Lips.png" width="4%" height="4%"> Pink Feelin'</a>
-                  <div class=" hide-on-med-and-down">
+                 <a href="#!" class="left brand-logo"><img class="responsive-img" src="images/Lips.png" width="4%" height="4%"> Pink Feelin'</a>
+                <a href="#" data-activates="nav-mobile" class="button-collapse right"><i class="material-icons">menu</i></a>
+                  <div class="row hide-on-med-and-down">
                       <div class="col push-s4 hoverable"><a href="login.php">Inicio</a></div>
                       <div class="col push-s4 hoverable"><a href="tienda.php">Tienda</a></div>
                       <div class="col push-s4 hoverable"><a href="ofertas.php">Ofertas</a></div>
@@ -12,19 +22,25 @@
                       <div class="col push-s4 hoverable"><a href="atencion.php">Atención al Cliente</a></div>
                       <div class="col push-s4 hoverable"><a href="login.php">Login</a></div>
                     </div>
-                  <ul class="side-nav" id="mobile-demo">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
+                  <ul class="side-nav" id="nav-mobile">
+                    <li><a href="login.php">Inicio</a></li>
+                    <li><a href="tienda.php">Tienda</a></li>
+                    <li><a href="ofertas.php">Ofertas</a></li>
+                    <li><a href="contacto.php">Contáctanos</a></li>
+                    <li><a href="atencion.php">Atención al Cliente</a></li>
+                    <li><a href="login.php">Login</a></li>
                   </ul>
             </div>
         </nav>
-        </div>
+        <!--</div>-->
     </body>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    
+    
     <script>
         $( document ).ready(function(){
-            
-        })
+            $(".button-collapse").sideNav();
+        });
     </script>    
 </html>
