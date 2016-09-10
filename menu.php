e08b4f3fd04e906311d98b54f7df5c2daa626f0d
marcoleon <paramore_malj@outlook.com>
2016-09-09 20:28:04 -0500

    pagina index, menu y logo

---
+++ menu.php
@@ -0,0 +1,30 @@
+<html lang="es">
+    <body>
+        <div class="navbar-fixed">
+        <nav class="pink lighten-3">
+            <div class="nav-wrapper">
+              <a href="#!" class="right brand-logo"><img class="responsive-img" src="images/Lips.png" width="4%" height="4%"> Pink Feelin'</a>
+                  <div class=" hide-on-med-and-down">
+                      <div class="col push-s4 hoverable"><a href="login.php">Inicio</a></div>
+                      <div class="col push-s4 hoverable"><a href="tienda.php">Tienda</a></div>
+                      <div class="col push-s4 hoverable"><a href="ofertas.php">Ofertas</a></div>
+                      <div class="col push-s4 hoverable" ><a href="contacto.php">Contáctanos</a></div>
+                      <div class="col push-s4 hoverable"><a href="atencion.php">Atención al Cliente</a></div>
+                      <div class="col push-s4 hoverable"><a href="login.php">Login</a></div>
+                    </div>
+                  <ul class="side-nav" id="mobile-demo">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                  </ul>
+            </div>
+        </nav>
+        </div>
+    </body>
+    <script>
+        $( document ).ready(function(){
+            
+        })
+    </script>    
+</html>
